e08482aae48bd1fd83cdc695fb468cea205b3999
= <heysamlee@gmail.com>
2020-08-07 22:58:54 -0700

    added slides link

---
+++ AudioPlayer/AudioPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AudioPlayer
+//
+//  Created by Samuel J. Lee on 8/7/20.
+//  Copyright © 2020 Samuel J. Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# intro-to-ios
+# Intro to iOS README
+
+**Slides**: https://docs.google.com/presentation/d/1HXc5ddt3BGkBF8vkHT8kvhfxhnHWau7aNylO6YoiW0I/edit?usp=sharing
